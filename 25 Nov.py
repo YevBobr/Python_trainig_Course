5a0860d41a07d3d9573964a6cef0d57d9747a26b
Admin <jevgenionu@hotmail.com>
2020-11-26 20:20:22 +0100

    First commit

---
+++ 25 Nov.py
@@ -0,0 +1,10 @@
+[1000,2000 ]
+list2 = [a,b,10,]
+list3= [[1,2,][5,6]]
+list = ['one' , 'two']
+print(list[0])
+list5 = [["John,1000"],[Alice,2000,][Bob,3000]]
+print(list[1])
+tuple_1 = (A,)
+Tuple2 = (a,b,c,"bag")
+
